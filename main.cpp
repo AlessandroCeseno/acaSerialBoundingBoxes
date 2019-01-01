06a42f0d5cb4fd766f03c22973194e05bfce73b3
alex <lean@trovaprezzi.it>
2019-01-01 17:28:44 +0100

    Inserito il log per il tempo di esecuzione

--- main.cpp
+++ main.cpp
@@ -9,11 +9,18 @@ int main()
     int i;
     BoundingBoxes boundingBoxes;
     printf("Start to work with SERIAL algorithm. \n");
+    clock_t start, stop;
+    start = clock();
     for (i=1; i<= NUMBERPAGES; i++)
     {
         printf("elaborate page %d \n",i);
         boundingBoxes.workOnThisPage(i);
     }
+    stop = clock();
+    double elapsed_seconds = ((double) stop - start )/CLOCKS_PER_SEC; // in seconds
+    printf("Time elapsed in seconds: %f \n", elapsed_seconds);
+    double elapsed_milliseconds = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("Time elapsed in milliseconds: %f \n", elapsed_milliseconds);
 
     return 0;
 }
