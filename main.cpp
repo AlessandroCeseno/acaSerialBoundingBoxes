288f378c6cf54a6a3197d940e4d922e5dddda490
alex <alex>
2019-01-17 17:30:29 +0100

    Amdahl’s law e settato il numero a 15 di pagine

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@ int main()
 {
     std::cout << "Hello Advanced Computer Architecture. The SERIAL algorithm. Here my smart and simple project that works on Computer Vision!" << std::endl;
 
+    double parallelizzable_time = 0.0;
+
     printf("Start to work with SERIAL algorithm. \n");
     double start_time = omp_get_wtime();
 
-    std::string directory_path = "../../dataFlyers150/" ;
+    std::string directory_path = "../../dataFlyers15/" ;
     const char * c = directory_path.c_str();
     BoundingBoxes bounding_boxes = BoundingBoxes(directory_path);
     DIR *dir;
@@ -22,15 +24,21 @@ int main()
         {
             if( strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0 )
             {
+                double partial_start_parallelizzable_time = omp_get_wtime();
                 bounding_boxes.workOnThisPage(ent->d_name);
+                double partial_end_parallelizzable_time = omp_get_wtime();
+                parallelizzable_time = parallelizzable_time + (partial_end_parallelizzable_time - partial_start_parallelizzable_time);
             }
         }
+
         closedir(dir);
     }
 
     double end_time = omp_get_wtime();
     double total_time = end_time-start_time;
     printf("Serial implementation, Seconds: %f ", total_time);
+
+    printf("For Amdahl’s law, parallelizzable_time: %f ", parallelizzable_time);
 
     return 0;
 }
